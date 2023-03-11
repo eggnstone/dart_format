a44af9dd8f80aaa1a28628d16472398e0a2b183f
eggnstone <eggnstone@users.noreply.github.com>
2023-03-11 20:45:18 +0100

    Updated dotlin.sh

--- FixIsEmpty.sh
+++
@@ -1,6 +0,0 @@
-echo Fix isEmpty
-
-sed -i 's/bool isEmpty()/bool get isEmpty/' "..\..\..\GitHub\dotlin\packages\dotlin\lib\src\kotlin\ranges\range.dt.g.dart"
-sed -i 's/@override bool isEmpty()/@override bool get isEmpty/' "..\..\..\GitHub\dotlin\packages\dotlin\lib\src\kotlin\ranges\ranges.dt.g.dart"
-sed -i 's/\.isEmpty()/\.isEmpty/g' "..\..\..\GitHub\dotlin\packages\dotlin\lib\src\kotlin\ranges\ranges.dt.g.dart"
-sed -i 's/\.isEmpty()/\.isEmpty/g' "..\..\..\GitHub\dotlin\packages\dotlin\lib\src\kotlin\ranges\ranges_ext.dt.g.dart"
--- dotlin.sh
+++ dotlin.sh
@@ -12,6 +12,14 @@ cp -r $SOURCE/levels $DEST
 cp -r $SOURCE/parts $DEST
 cp -r $SOURCE/splitters $DEST
 
+#mkdir $DEST/splitters
+#cp $SOURCE/splitters/ISplitter.kt $DEST/splitters
+#cp $SOURCE/splitters/LineSplitter.kt $DEST/splitters
+#cp $SOURCE/splitters/TypeSplitter.kt $DEST/splitters
+#cp $SOURCE/splitters/StringSplitter.kt $DEST/splitters
+#cp $SOURCE/splitters/SplitType.kt $DEST/splitters
+#cp $SOURCE/splitters/SplitResult.kt $DEST/splitters
+
 cp $SOURCE/Constants.kt $DEST
 cp $SOURCE/DartFormatException.kt $DEST
 cp $SOURCE/Tools.kt $DEST
@@ -20,7 +28,7 @@ echo Replacing some files ...
 cp $DEST/dotlin/DotlinLogger.kt.save $DEST/dotlin/DotlinLogger.kt
 
 echo Executing Dotlin ...
-../../../GitHub/dotlin/compiler/build/distributions/dotlin-0.0.1/bin/dotlin
+../../../GitHub/dotlin/compiler/build/distributions/dotlin-0.0.1/bin/dotlin || exit
 
 echo Replacing some files ...
 cp $DEST/dart_format_exception.dt.g.dart.save $DEST/dart_format_exception.dt.g.dart
@@ -28,6 +36,4 @@ cp $DEST/dotlin/dotlin_logger.dt.g.dart.save  $DEST/dotlin/dotlin_logger.dt.g.da
 mv $DEST/dotlin/dotlin_tools.dt.g.dart        $DEST/dotlin/dotlin_tools.dt.g.dart.old
 
 echo Adding imports ...
-#echo 'import "../StringExtensions.dart";' | cat - $DEST/dotlin/dotlin_tools.dt.g.dart.old > $DEST/dotlin/dotlin_tools.dt.g.dart
-
-#./FixIsEmpty.sh
+echo 'import "../StringExtensions.dart";' | cat - $DEST/dotlin/dotlin_tools.dt.g.dart.old > $DEST/dotlin/dotlin_tools.dt.g.dart
