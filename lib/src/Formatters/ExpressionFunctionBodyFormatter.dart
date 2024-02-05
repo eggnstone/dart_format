44570973694da64266d249844bbab7983789b98b
eggnstone <eggnstone@users.noreply.github.com>
2024-02-05 15:38:52 +0700

    Fixed ExpressionFunctionBodyFormatter (keyword).

--- lib/src/Formatters/ExpressionFunctionBodyFormatter.dart
+++ lib/src/Formatters/ExpressionFunctionBodyFormatter.dart
@@ -23,6 +23,7 @@ class ExpressionFunctionBodyFormatter extends IFormatter
         if (node is! ExpressionFunctionBody)
             throw FormatException('Not an ExpressionFunctionBody: ${node.runtimeType}');
 
+        formatState.copyEntity(node.keyword, astVisitor, '$methodName/node.keyword');
         formatState.copyEntity(node.functionDefinition, astVisitor, '$methodName/node.functionDefinition');
         formatState.copyEntity(node.expression, astVisitor, '$methodName/node.expression');
         formatState.copySemicolon(node.semicolon, config, '$methodName/node.semicolon');
