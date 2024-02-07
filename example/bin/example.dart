752dd786a93da0e338b78aa99f68cff307e4aa9c
eggnstone <eggnstone@users.noreply.github.com>
2024-02-07 16:01:53 +0700

    Added example.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,6 +1,14 @@
-import 'package:example/example.dart' as example;
+// ignore_for_file: avoid_print
+
+import 'package:dart_format/dart_format.dart';
 
 void main(List<String> arguments)
 {
-    print('Hello world: ${example.calculate()}!');
+    const String unformattedText = 'class C{void m(){print("Hello world");}}';
+    print('Unformatted text:\n$unformattedText\n');
+
+    const Config config = Config.all();
+    final Formatter formatter = Formatter(config);
+    final String formattedText = formatter.format(unformattedText);
+    print('Formatted text:\n$formattedText\n');
 }
