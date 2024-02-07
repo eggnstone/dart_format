72a6597619826b50e70607726f813d41334b48a1
eggnstone <eggnstone@users.noreply.github.com>
2024-02-07 16:37:32 +0700

    v1.1.8: Added example. Improved documentation.

--- lib/src/Constants/Generated/VersionConstants.dart
+++ lib/src/Constants/Generated/VersionConstants.dart
@@ -4,7 +4,7 @@ class VersionConstants
 {
     static const int MAJOR = 1;
     static const int MINOR = 1;
-    static const int PATCH = 7;
+    static const int PATCH = 8;
 
     static const Version VERSION = Version(MAJOR, MINOR, PATCH);
 }
