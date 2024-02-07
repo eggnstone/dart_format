f5a189f00d6c7ea4d51309944edfef8193b0b02b
eggnstone <eggnstone@users.noreply.github.com>
2024-02-07 15:53:10 +0700

    Adding example.

---
+++ example/bin/example.dart
@@ -0,0 +1,6 @@
+import 'package:example/example.dart' as example;
+
+void main(List<String> arguments)
+{
+    print('Hello world: ${example.calculate()}!');
+}
---
+++ example/test/example_test.dart
@@ -0,0 +1,11 @@
+import 'package:example/example.dart';
+import 'package:test/test.dart';
+
+void main()
+{
+    test('calculate', ()
+        {
+            expect(calculate(), 42);
+        }
+    );
+}
