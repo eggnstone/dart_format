b78757166274bc4185089fd44d6d82203ddc5bd4
eggnstone <eggnstone@users.noreply.github.com>
2024-02-25 12:22:16 +0700

    Cleanup.

--- lib/src/Formatters/ContinueStatementFormatter.dart
+++ lib/src/Formatters/ContinueStatementFormatter.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: always_put_control_body_on_new_line
 
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:eggnstone_dart/eggnstone_dart.dart';
 
 import '../Config.dart';
 import '../Constants/Constants.dart';
