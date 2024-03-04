f9261840ce979f72cfa1e440d2bcd07447ce8ce3
eggnstone <eggnstone@users.noreply.github.com>
2024-03-04 14:27:43 +0700

    Updated version.

--- lib/src/Constants/Generated/VersionConstants.dart
+++ lib/src/Constants/Generated/VersionConstants.dart
@@ -4,7 +4,7 @@ class VersionConstants
 {
     static const int MAJOR = 1;
     static const int MINOR = 1;
-    static const int PATCH = 12;
+    static const int PATCH = 13;
 
     static const Version VERSION = Version(MAJOR, MINOR, PATCH);
 }
