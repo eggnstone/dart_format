14c4b4aa4753643782056349d64e32eeb11410a9
Mark Eggenstein <mark.eggenstein@eggnstone.com>
2024-12-14 13:02:57 +0700

    v1.2.1: Update analyzer to 6.9.0.

--- lib/src/Constants/Generated/VersionConstants.dart
+++ lib/src/Constants/Generated/VersionConstants.dart
@@ -4,7 +4,7 @@ class VersionConstants
 {
     static const int MAJOR = 1;
     static const int MINOR = 2;
-    static const int PATCH = 0;
+    static const int PATCH = 1;
 
     static const Version VERSION = Version(MAJOR, MINOR, PATCH);
 }
