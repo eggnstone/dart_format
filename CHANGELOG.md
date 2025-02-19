042b8ef11f68feecdc30da0a29e1c1f1056b32c8
eggnstone <eggnstone@users.noreply.github.com>
2025-02-19 14:06:25 +0100

    v1.3.3: Improved fixing of spaces.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 1.3.2
+## 1.3.3
 
 - Improved fixing of spaces.
 
--- lib/src/Constants/Generated/VersionConstants.dart
+++ lib/src/Constants/Generated/VersionConstants.dart
@@ -4,7 +4,7 @@ class VersionConstants
 {
     static const int MAJOR = 1;
     static const int MINOR = 3;
-    static const int PATCH = 2;
+    static const int PATCH = 3;
 
     static const Version VERSION = Version(MAJOR, MINOR, PATCH);
 }
