baa4efa143cbd43e79e6b43ac89217234ff9ffbe
eggnstone <eggnstone@users.noreply.github.com>
2025-02-22 12:52:20 +0100

    Fixed commas in comments.

--- lib/src/Constants/Constants.dart
+++ lib/src/Constants/Constants.dart
@@ -3,10 +3,11 @@ class Constants
     static const bool DEBUG = false;
     static const bool DEBUG_ALL = false;
 
+    static const bool DEBUG_COMMENT_TOOLS = (DEBUG && false) || DEBUG_ALL;
     static const bool DEBUG_DART_FORMAT_HANDLERS = (DEBUG && false) || DEBUG_ALL;
     static const bool DEBUG_FORMAT_STATE = (DEBUG && false) || DEBUG_ALL;
     static const bool DEBUG_FORMAT_STATE_SPACING = (DEBUG && false) || DEBUG_ALL;
-    static const bool DEBUG_FORMAT_TOOLS = DEBUG && false; // || DEBUG_ALL;
+    static const bool DEBUG_FORMAT_TOOLS = (DEBUG && false) || DEBUG_ALL;
     static const bool DEBUG_FORMATTER = (DEBUG && false) || DEBUG_ALL;
     static const bool DEBUG_FORMATTER_DEFAULT = (DEBUG && false) || DEBUG_ALL;
     static const bool DEBUG_I_FORMATTER = (DEBUG && false) || DEBUG_ALL;
