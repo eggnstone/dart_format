bd5585d1ff7c1544f342e68a1d7ce43f23db2ad7
eggnstone <eggnstone@users.noreply.github.com>
2025-02-22 13:17:50 +0100

    v1.4.0: Added "Fix spaces" to official config.

--- lib/src/Constants/Generated/VersionConstants.dart
+++ lib/src/Constants/Generated/VersionConstants.dart
@@ -3,8 +3,8 @@ import '../../Data/Version.dart';
 class VersionConstants
 {
     static const int MAJOR = 1;
-    static const int MINOR = 3;
-    static const int PATCH = 3;
+    static const int MINOR = 4;
+    static const int PATCH = 0;
 
     static const Version VERSION = Version(MAJOR, MINOR, PATCH);
 }
