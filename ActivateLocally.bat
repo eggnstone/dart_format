e1477c83a4169e0e26d465555beba2e2989da497
eggnstone <eggnstone@users.noreply.github.com>
2026-05-14 10:13:08 +0200

    Fixed ActivateLocally.bat

--- ActivateLocally.bat
+++ ActivateLocally.bat
@@ -1,4 +1,4 @@
 @echo off
 del /q .dart_tool\pub\bin\dart_format\dart_format.dart-*.snapshot 2>nul
-dart pub upgrade
-dart pub global activate --source path . --overwrite
+call dart pub upgrade
+call dart pub global activate --source path . --overwrite
